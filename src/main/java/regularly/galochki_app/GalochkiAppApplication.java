2ca7966d56f878ede208dc978604bf6a9dcd31ec
egor-no <e.nosko93@ya.ru>
2025-03-23 22:19:00 +0500

    Initial galochki commit

---
+++ src/main/java/regularly/galochki_app/GalochkiAppApplication.java
@@ -0,0 +1,13 @@
+package regularly.galochki_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalochkiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalochkiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/regularly/galochki_app/GalochkiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package regularly.galochki_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalochkiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
