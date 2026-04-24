2b804936a111bdafc3f61206b8abb837ab8b216c
egor-no <e.nosko93@ya.ru>
2026-04-24 13:19:45 +0500

    Fresh start

---
+++ src/main/java/dev/egor/galochkiapp/GalochkiAppApplication.java
@@ -0,0 +1,13 @@
+package dev.egor.galochkiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalochkiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalochkiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/egor/galochkiapp/GalochkiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.egor.galochkiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalochkiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
